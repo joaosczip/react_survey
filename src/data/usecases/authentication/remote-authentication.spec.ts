624323bab82690d0ee69f967c3469f41da51ede3
João Guilherme <joao.sczip@hotmail.com>
2020-06-27 13:32:47 -0300

    feat: move HttpPostClientSpy to its own file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,14 +1,5 @@
-import { HttpPostClient } from '../../protocols/http/http-post-client';
 import { RemoteAuthentication } from './remote-authentication';
-
-class HttpPostClientSpy implements HttpPostClient {
-  url?: string;
-
-  post(url: string): Promise<void> {
-    this.url = url;
-    return Promise.resolve();
-  }
-}
+import { HttpPostClientSpy } from '../../test/mock-http-client';
 
 describe('RemoteAuthentication', () => {
   it('should call HttpPostClient with correct URL', async () => {
