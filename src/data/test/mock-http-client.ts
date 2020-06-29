dd8b311b2d35819fd5e8ca47640b042dadfd9d40
João Guilherme <joao.sczip@hotmail.com>
2020-06-29 19:40:21 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -3,8 +3,8 @@ import { HttpPostClient } from '../protocols/http/http-post-client';
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
 
-  post(url: string): Promise<void> {
-    this.url = url;
+  post(params: HttpPostClient.Params): Promise<void> {
+    this.url = params.url;
     return Promise.resolve();
   }
 }
