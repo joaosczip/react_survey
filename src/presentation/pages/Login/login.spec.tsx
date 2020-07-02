176528ba3fbfd74875e9bb6e3083da8f0ce67302
João Guilherme <joao.sczip@hotmail.com>
2020-07-01 22:44:02 -0300

    test: ensure Login not render spinner and error on start

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { useContext } from 'react';
+import { render, screen } from '@testing-library/react';
 
 import Login from './';
 import ThemeProvider from '@/presentation/components/ThemeProvider';
+import FormContext from '@/presentation/contexts/form/form-context';
 
 const Sut = () => {
   return (
@@ -13,7 +14,9 @@ const Sut = () => {
 };
 
 describe('LoginPage', () => {
-  it('should ', () => {
+  it('should not render spinner and error on start', () => {
     render(<Sut />);
+    const errorContainer = screen.getByTestId('error-container');
+    expect(errorContainer.childElementCount).toBe(0);
   });
 });
