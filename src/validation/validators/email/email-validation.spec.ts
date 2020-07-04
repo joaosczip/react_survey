d82af18e1ad29e70f1876513c16c28c3b26aa8ed
João Guilherme <joao.sczip@hotmail.com>
2020-07-04 11:28:30 -0300

    feat: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,4 +1,6 @@
 import { FieldValidation } from '@/validation/protocols/field-validation';
+import faker from 'faker';
+
 import { InvalidFieldError } from '@/validation/errors';
 import { EmailValidation } from './email-validation';
 
@@ -7,7 +9,12 @@ const makeSut = () => new EmailValidation('email');
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
     const sut = makeSut();
-    const error = sut.validate('invalid_email');
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError(sut.field));
+  });
+  it('should return falsy if email is valid', () => {
+    const sut = makeSut();
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
