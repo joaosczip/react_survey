3b80db568c4584784e57c1c5a358be5606557c52
João Guilherme <joao.sczip@hotmail.com>
2020-07-06 21:34:05 -0300

    test: ensure ValidationComposite uses correct validations

---
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { makeLoginValidation } from './login-validation-factory';
+import {
+  ValidationComposite,
+  ValidationBuilder,
+} from '@/validation/validators';
+
+describe('LoginValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const composite = makeLoginValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+      ])
+    );
+  });
+});
