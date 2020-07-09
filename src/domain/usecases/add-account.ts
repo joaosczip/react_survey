65172a15c554b8e0bfd34b30bcdc2d2d2a507954
João Guilherme <joao.sczip@hotmail.com>
2020-07-09 20:15:07 -0300

    feat: add AddAccount usecase

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '@/domain/models/account-model';
+
+export interface AddAccount {
+  add: (params: AddAccount.Params) => Promise<AccountModel>;
+}
+
+export namespace AddAccount {
+  export type Params = {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+  };
+}
