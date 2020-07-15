b051345e2e379b3d508d29a5f876cd25eca81b03
João Guilherme <joao.sczip@hotmail.com>
2020-07-15 20:10:08 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,13 @@
 describe('Name of the group', () => {
-  it('should visit login', () => {
-    cy.visit('login');
+  beforeEach(() => cy.visit('login'));
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', 'Error');
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', 'Error');
+    cy.getByTestId('submit').should('be.disabled');
+    cy.getByTestId('error-container').should('not.have.descendants');
   });
 });
