0a88e6db3e9faf73d5e865bbfcb374b35189cb9e
João Guilherme <joao.sczip@hotmail.com>
2020-07-16 21:35:58 -0300

    test: ensure SignUp present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 import * as FormHelper from '../support/form-helper';
 
 describe('SignUp', () => {
@@ -15,5 +17,21 @@ describe('SignUp', () => {
     FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório');
     cy.getByTestId('submit').should('be.disabled');
     cy.getByTestId('error-container').should('not.have.descendants');
+  });
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(2));
+    FormHelper.testInputStatus('name', 'Campo name inválido');
+    cy.getByTestId('email').focus().type(faker.random.word());
+    FormHelper.testInputStatus('email', 'Campo email inválido');
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(4));
+    FormHelper.testInputStatus('password', 'Campo password inválido');
+    cy.getByTestId('passwordConfirmation')
+      .focus()
+      .type(faker.random.alphaNumeric(4));
+    FormHelper.testInputStatus(
+      'passwordConfirmation',
+      'Campo passwordConfirmation inválido'
+    );
+    cy.getByTestId('submit').should('be.disabled');
   });
 });
