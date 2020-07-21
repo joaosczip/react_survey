5e27b90a51ecc1be022dcaec7b8e87c3d75a8bc7
João Guilherme <joao.sczip@hotmail.com>
2020-07-20 23:45:55 -0300

    feat: apply PrivateRoute to SurveyList page

--- src/main/routes/route.tsx
+++ src/main/routes/route.tsx
@@ -11,6 +11,7 @@ import {
   getCurrentAccountAdapter,
 } from '@/main/adapters/current-account-adapter';
 import SurveyList from '../../presentation/pages/SurveyList';
+import PrivateRoute from '@/presentation/components/PrivateRoute';
 
 const Router: React.FC = () => {
   return (
@@ -25,7 +26,7 @@ const Router: React.FC = () => {
           <Switch>
             <Route path="/login" exact component={makeLogin} />
             <Route path="/signup" exact component={makeSignUp} />
-            <Route path="/" exact component={SurveyList} />
+            <PrivateRoute path="/" exact component={SurveyList} />
           </Switch>
         </BrowserRouter>
         <GlobalStyle />
--- src/presentation/components/PrivateRoute/private-route.spec.tsx
+++ src/presentation/components/PrivateRoute/private-route.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 
