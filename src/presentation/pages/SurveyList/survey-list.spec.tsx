bda00a6ea03b0b55375488fc7991a4bf948f74a7
João Guilherme <joao.sczip@hotmail.com>
2020-07-23 22:39:04 -0300

    test: ensure SurveyList render SurveyItem on success

--- src/presentation/pages/SurveyList/survey-list.spec.tsx
+++ src/presentation/pages/SurveyList/survey-list.spec.tsx
@@ -5,12 +5,14 @@ import SurveyList from '.';
 import ThemeProvider from '@/presentation/components/ThemeProvider';
 import { LoadSurveyList } from '@/domain/usecases';
 import { SurveyModel } from '@/domain/models';
+import { makeSurveyListModel } from '@/domain/test';
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0;
+  surveys = makeSurveyListModel();
   async loadAll(): Promise<SurveyModel[]> {
     this.callsCount++;
-    return [];
+    return this.surveys;
   }
 }
 
@@ -33,10 +35,15 @@ describe('SurveyList', () => {
   it('should 4 empty items on start', () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
   });
   it('should calls LoadSurveyList', () => {
     const { loadSurveyListSpy } = makeSut();
     expect(loadSurveyListSpy.callsCount).toBe(1);
+  });
+  it('should render SurveyItems on success', async () => {
+    makeSut();
+    const surveyList = await screen.findByTestId('survey-list');
+    expect(surveyList.querySelectorAll('li:not(:empty)')).toHaveLength(2);
   });
 });
