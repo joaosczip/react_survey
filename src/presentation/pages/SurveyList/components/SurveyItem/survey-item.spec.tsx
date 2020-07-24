1193f3f621f8b476efc7bd3207e6503472d4bae5
João Guilherme <joao.sczip@hotmail.com>
2020-07-23 22:15:11 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/SurveyList/components/SurveyItem/survey-item.spec.tsx
+++ src/presentation/pages/SurveyList/components/SurveyItem/survey-item.spec.tsx
@@ -3,32 +3,25 @@ import { render, screen } from '@testing-library/react';
 
 import SurveyItem from '.';
 import { mockSurveyModel } from '@/domain/test';
-import { SurveyModel } from '@/domain/models';
 import ThemeProvider from '@/presentation/components/ThemeProvider';
 import { IconName } from '@/presentation/components/Icon';
 
-type SutTypes = {
-  surveyModel: SurveyModel;
-};
-
-const makeSut = (): SutTypes => {
-  const surveyModel = mockSurveyModel();
-  surveyModel.didAnswer = true;
-  surveyModel.date = new Date('2020-01-10T00:00:00');
+const makeSut = (surveyModel = mockSurveyModel()): void => {
   render(
     <ThemeProvider>
       <SurveyItem survey={surveyModel} />
     </ThemeProvider>
   );
-
-  return {
-    surveyModel,
-  };
 };
 
 describe('SurveyItem', () => {
   it('should render with correct values', () => {
-    const { surveyModel } = makeSut();
+    const surveyModel = {
+      ...mockSurveyModel(),
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00'),
+    };
+    makeSut(surveyModel);
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(
       surveyModel.question
