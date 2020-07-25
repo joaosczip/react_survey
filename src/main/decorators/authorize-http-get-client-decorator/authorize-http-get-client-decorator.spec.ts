71da181058450b461130381d14508325af011572
João Guilherme <joao.sczip@hotmail.com>
2020-07-25 11:15:33 -0300

    test: ensure AuthorizeHttpGetClientDecorator returns the same as HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -73,4 +73,9 @@ describe('AuthorizeHttpGetClientDecorator', () => {
       'x-access-token': getStorageSpy.value.accessToken,
     });
   });
+  it('should return the same result as HttpGetClient', async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    const httpResponse = await sut.get(mockGetRequest());
+    expect(httpResponse).toEqual(httpGetClientSpy.response);
+  });
 });
