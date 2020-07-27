30150a2192e8354b29c987af5a3e49ecdcff46cc
João Guilherme <joao.sczip@hotmail.com>
2020-07-27 19:52:07 -0300

    test: ensure SurveyList logout on AccessDeniedError

--- src/presentation/pages/SurveyList/survey-list.spec.tsx
+++ src/presentation/pages/SurveyList/survey-list.spec.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, History } from 'history';
 
 import SurveyList from '.';
 import ThemeProvider from '@/presentation/components/ThemeProvider';
 import { LoadSurveyList } from '@/domain/usecases';
 import { makeSurveyListModel, mockAccountModel } from '@/domain/test';
-import { UnexpectedError } from '@/domain/errors';
+import { UnexpectedError, AccessDeniedError } from '@/domain/errors';
 import { ApiContext } from '@/presentation/contexts/api/api-context';
+import { AccountModel } from '@/domain/models';
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0;
@@ -21,15 +22,19 @@ class LoadSurveyListSpy implements LoadSurveyList {
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy;
+  history: History;
+  setCurrentAccountMock: (account: AccountModel) => Promise<void>;
 };
 
 const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
+  const history = createMemoryHistory();
+  const setCurrentAccountMock = jest.fn();
   render(
     <ThemeProvider>
-      <Router history={createMemoryHistory()}>
+      <Router history={history}>
         <ApiContext.Provider
           value={{
-            setCurrentAccount: jest.fn(),
+            setCurrentAccount: setCurrentAccountMock,
             getCurrentAccount: () => mockAccountModel(),
           }}
         >
@@ -39,7 +44,7 @@ const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
     </ThemeProvider>
   );
 
-  return { loadSurveyListSpy };
+  return { loadSurveyListSpy, history, setCurrentAccountMock };
 };
 
 describe('SurveyList', () => {
@@ -58,7 +63,7 @@ describe('SurveyList', () => {
     expect(surveyList.querySelectorAll('li:not(:empty)')).toHaveLength(2);
     expect(screen.queryByTestId('error')).not.toBeInTheDocument();
   });
-  it('should render error on failure', async () => {
+  it('should render error on UnexpectedError', async () => {
     const loadSurveyListSpy = new LoadSurveyListSpy();
     jest
       .spyOn(loadSurveyListSpy, 'loadAll')
@@ -70,6 +75,17 @@ describe('SurveyList', () => {
     expect(await screen.findByTestId('error')).toHaveTextContent(
       'Algo de errado aconteceu! Tente novamente.'
     );
+  });
+  it('should logout on AccessDeniedError', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy();
+    jest
+      .spyOn(loadSurveyListSpy, 'loadAll')
+      .mockRejectedValueOnce(new AccessDeniedError());
+    const { history, setCurrentAccountMock } = makeSut(loadSurveyListSpy);
+    await waitFor(() =>
+      expect(setCurrentAccountMock).toHaveBeenCalledWith(null)
+    );
+    expect(history.location.pathname).toBe('/login');
   });
   it('should calls LoadSurveyList on reload', async () => {
     const loadSurveyListSpy = new LoadSurveyListSpy();
